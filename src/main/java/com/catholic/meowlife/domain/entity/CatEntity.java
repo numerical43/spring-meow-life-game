b3cf7b6ee393c1818982ac441dce953deb4a18a2
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-06-15 19:13:47 +0900

    고양이, 플레이어 entity 추가

--- src/main/java/com/catholic/meowlife/domain/entity/CatEntity.java
+++ src/main/java/com/catholic/meowlife/domain/entity/CatEntity.java
@@ -1,4 +1,18 @@
 package com.catholic.meowlife.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class CatEntity {
+    private String catName;
+    private String catBreeds;
+    private int energy;
+    private int weight;
+    private int exp;
 }
--- src/main/java/com/catholic/meowlife/domain/entity/PlayerEntity.java
+++ src/main/java/com/catholic/meowlife/domain/entity/PlayerEntity.java
@@ -1,4 +1,16 @@
 package com.catholic.meowlife.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class PlayerEntity {
+    private String id;
+    private String pw;
+    private String name;
 }
