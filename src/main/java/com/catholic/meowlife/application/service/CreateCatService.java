e9fb9106a9e10ea1eab019281a7f6c50eb8994a9
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-06-18 17:26:51 +0900

    1. 고양이 생성 구현 2. 머지할때 생성 완료 후 게임 컨트롤러 이동 추가 요망

--- src/main/java/com/catholic/meowlife/application/service/CreateCatService.java
+++ src/main/java/com/catholic/meowlife/application/service/CreateCatService.java
@@ -1,4 +1,18 @@
 package com.catholic.meowlife.application.service;
 
+import com.catholic.meowlife.domain.repository.CatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreateCatService {
+    @Autowired
+    CatRepository catRepository;
+
+    public void createCat(String catName, String catBreeds) {
+        catRepository.createCatInDB(catName,catBreeds);
+
+        // 머지 이후 게임 컨트롤러(게임 컨트롤러? 게임 화면 뷰?)로 이동 추가
+
+    }
 }
--- src/main/java/com/catholic/meowlife/domain/repository/CatRepository.java
+++ src/main/java/com/catholic/meowlife/domain/repository/CatRepository.java
@@ -1,4 +1,22 @@
 package com.catholic.meowlife.domain.repository;
 
+import com.catholic.meowlife.domain.entity.CatEntity;
+import com.catholic.meowlife.domain.entity.PlayerEntity;
+import com.catholic.meowlife.infra.DB.CatDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CatRepository {
+    @Autowired
+    CatEntity catEntity;
+    @Autowired
+    PlayerEntity playerEntity;
+
+    public void createCatInDB(String catName, String catBreeds) {
+        playerEntity = new PlayerEntity("qlql", "1234", "홍길동");
+        catEntity = new CatEntity(catName, catBreeds, 50, 2, 0, 1);
+
+        CatDB.getCatMap().put(playerEntity.getId(), catEntity);
+    }
 }
