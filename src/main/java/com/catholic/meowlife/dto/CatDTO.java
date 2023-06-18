c027c828a1dd3d5f00f5e5c85ba02e4cc3d036bd
Ra Hyeon Ji <119282494+raxchaz@users.noreply.github.com>
2023-06-18 21:33:03 +0900

    weight, exp, level Bug Fix2

--- src/main/java/com/catholic/meowlife/dto/CatDTO.java
+++ src/main/java/com/catholic/meowlife/dto/CatDTO.java
@@ -1,4 +1,19 @@
 package com.catholic.meowlife.dto;
 
-public class CatDTO {
-}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @ToString
+    public class CatDTO {
+        private String catName;
+        private String catBreeds;
+        private int energy;
+        private double weight;
+        private int exp;
+        private int level;
+    }
